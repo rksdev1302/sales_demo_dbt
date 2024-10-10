8477c23ee0f436bd843ea0330ebe9f5b0a66b2ba
rksol <rksolanke1997@gmail.com>
2024-10-10 16:04:53 +0530

    first Commit

---
+++ models/example/my_first_dbt_model.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table'
+ ) }}
+
+with source_data as (
+
+    select 1 as id
+    union all
+    select null as id
+    union all
+    select 2 as id
+
+)
+
+select id
+from source_data
